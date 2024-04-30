23a964446c8ec6bc18fde15c1e02ce891ea7565e
zsGBB <zacksoenen@microsoft.com>
2024-04-30 12:05:24 -0500

    updated eval pf to run on remote compute

--- src/evaluation/test_chat_quality.py
+++ src/evaluation/test_chat_quality.py
@@ -1,20 +1,31 @@
 from promptflow.client import PFClient
+from azure.identity import DefaultAzureCredential
+import os
 
 
 def quality_test(): 
+  
   # PFClient can help manage your runs and connections.
-  pf = PFClient()
-
+  pf = PFClient(credential=DefaultAzureCredential(),
+                subscription_id=os.environ.get('SUBSCRIPTION_ID'),
+                resource_group_name=os.environ.get('RESOURCE_GROUP_NAME'),
+                workspace_name=os.environ.get('WORKSPACE_NAME')
+                )
+  
   # Define Flows and Data
   chat_flow = "./src/promptflows/chat_rag_wiki" # set the flow directory
   eval_flow = "./src/evaluation/eval_flow" # set flow directory
   data = "./src/evaluation/input_test_data.csv" # set the data file
+
+  # Define runtime and resources
+  resources = {"compute": "remote-cpu-cluster"}
 
   # Run chat flow to generate chat results
   rag_chat_run = pf.run(
       flow=chat_flow,
       data=data,
       stream=False,
+      resources=resources,
       column_mapping={  # map the url field from the data to the url input of the flow
         "input": "${data.input}",
       }
@@ -26,6 +37,7 @@ def quality_test():
       data=data,
       run=rag_chat_run,
       stream=False,
+      resources=resources,
       column_mapping={  # map the url field from the data to the url input of the flow
         "question": "${data.input}",
         "answer": "${run.outputs.answer}",
