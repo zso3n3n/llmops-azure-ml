d216e926c8ce8c339e502275d136559631f32443
zsGBB <zacksoenen@microsoft.com>
2024-05-08 17:54:37 -0500

    updated monitoring and metric gathering module. Added deployment with data collection on

--- src/promptflows/chat_rag_wiki/prompt_shield.py
+++ src/promptflows/chat_rag_wiki/prompt_shield.py
@@ -8,22 +8,26 @@ import os
 def prompt_shield(prompt: str, context: str):
     # Send a request to the URL
     url = f"{os.environ.get('CONTENT_SAFE_BASE')}contentsafety/text:shieldPrompt?api-version=2024-02-15-preview"
+    try:
+        headers = {
+                "Ocp-Apim-Subscription-Key": os.environ.get("CONTENT_SAFE_KEY"),
+                "Content-Type": "application/json",
+                }
+        
+        body = {
+                "userPrompt": prompt,
+                "documents": [context]
+                }
+        
+        response = requests.post(url=url, headers=headers, json=body)
+        promptAttackDetected = response.json()["userPromptAnalysis"]['attackDetected']
 
-    headers = {
-            "Ocp-Apim-Subscription-Key": os.environ.get("CONTENT_SAFE_KEY"),
-            "Content-Type": "application/json",
-            }
+        return promptAttackDetected
     
-    body = {
-            "userPrompt": prompt,
-            "documents": [context]
-            }
+    except Exception as e:
+        print("Get url failed with error: {}".format(e))
+        return
     
-    response = requests.post(url=url, headers=headers, json=body)
-    promptAttackDetected = response.json()["userPromptAnalysis"]['attackDetected']
-    print(type(promptAttackDetected))
-
-    return promptAttackDetected
 
 if __name__ == "__main__":
     prompt_shield("What is the capital of France?", "Paris")
