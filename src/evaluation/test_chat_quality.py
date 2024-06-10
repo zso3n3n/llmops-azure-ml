3938ce89b3f7ebef3fb0119a22cb84ad3a1f2ecd
zsGBB <zacksoenen@microsoft.com>
2024-06-10 12:10:46 -0500

    github actions cleanup - updated with prompt sheild and ragas

--- src/evaluation/test_chat_quality.py
+++ src/evaluation/test_chat_quality.py
@@ -7,7 +7,6 @@ import os
 
 def quality_test(): 
   
-  # PFClient can help manage your runs and connections.
   pf = PFClient(credential=DefaultAzureCredential(),
                 subscription_id=os.environ.get('SUBSCRIPTION_ID'),
                 resource_group_name=os.environ.get('RESOURCE_GROUP_NAME'),
@@ -19,23 +18,30 @@ def quality_test():
   eval_flow = "./src/evaluation/eval_flow" # set flow directory
   data = "./src/evaluation/input_test_data.csv" # set the data file
 
-  # Define runtime and resources
+  # Define remote compute instance (serverless)
   resources = {"instance_type": "Standard_D2"}
 
  ##### Run chat flow #########
   chat_run = Run(
-      display_name="Chat RAG Wiki Run",
-      flow=chat_flow,
-      data=data,
-      resources=resources,
-      column_mapping={  # map the url field from the data to the url input of the flow
-        "input": "${data.input}",
-      }
+    display_name="Chat Run - Python",
+    flow=chat_flow,
+    data=data,
+    resources=resources,
+    column_mapping={  # map the url field from the data to the url input of the flow
+    "input": "${data.input}",
+    },
+    environment_variables={
+    "CONTENT_SAFE_BASE": os.environ.get('CONTENT_SAFE_BASE'),
+    "CONTENT_SAFE_KEY": os.environ.get('CONTENT_SAFE_KEY'),
+    "AZURE_OPENAI_API_KEY": os.environ.get('AZURE_OPENAI_API_KEY'),
+    "AZURE_OPENAI_ENDPOINT": os.environ.get('AZURE_OPENAI_ENDPOINT'),
+    "AZURE_OPENAI_DEPLOYMENT_NAME": os.environ.get('AZURE_OPENAI_DEPLOYMENT_NAME')
+    }
   )
 
   chat_run_job = pf.runs.create_or_update(
     run=chat_run,
-)
+  )
   
   pf.runs.stream(chat_run_job) # This is important - essentially a wait function
 
@@ -43,11 +49,10 @@ def quality_test():
 
  ##### Run eval flow  #########
   eval_run = Run(
-      display_name="Eval RAG Wiki Run",
+      display_name="Eval Run - Python",
       flow=eval_flow,
       data=data,
       run=chat_run,
-      stream=False,
       resources=resources,
       column_mapping={  # map the url field from the data to the url input of the flow
         "question": "${data.input}",
